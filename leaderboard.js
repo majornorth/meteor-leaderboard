b95ab33ba661c8b0258a0784988b46760e70cbad
Stewart McCoy <stewart@chegg.com>
2014-11-28 00:47:02 -0800

    code as of Chapter 8

---
+++ leaderboard.js
@@ -0,0 +1,29 @@
+PlayersList = new Mongo.Collection('players');
+
+console.log("Hello world");
+
+if(Meteor.isClient){
+	Template.leaderboard.helpers({
+		'player': function(){
+			return PlayersList.find()
+		},
+		'selectedClass': function(){
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if(playerId == selectedPlayer){
+				return "selected"
+			}
+		}
+	});
+
+	Template.leaderboard.events({
+		'click .player': function(){
+			var playerId = this._id;
+			Session.set('selectedPlayer', playerId);
+		}
+	});
+}
+
+if(Meteor.isServer){
+
+}
