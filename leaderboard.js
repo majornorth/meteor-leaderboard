a4460758eb71c845c18262b6971d019459b4d8ce
Stewart McCoy <stewart@chegg.com>
2014-11-29 16:35:59 -0800

    code up to chapter 9

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,7 @@ console.log("Hello world");
 if(Meteor.isClient){
 	Template.leaderboard.helpers({
 		'player': function(){
-			return PlayersList.find()
+			return PlayersList.find({}, {sort: {score: -1}, name: 1 })
 		},
 		'selectedClass': function(){
 			var playerId = this._id;
@@ -13,6 +13,10 @@ if(Meteor.isClient){
 			if(playerId == selectedPlayer){
 				return "selected"
 			}
+		},
+		'showSelectedPlayer': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+			return PlayersList.findOne(selectedPlayer)
 		}
 	});
 
@@ -20,6 +24,14 @@ if(Meteor.isClient){
 		'click .player': function(){
 			var playerId = this._id;
 			Session.set('selectedPlayer', playerId);
+		},
+		'click .increment': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+		},
+		'click .decrement': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, {$inc: {score: -5}});
 		}
 	});
 }
